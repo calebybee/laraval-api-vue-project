e77a454cab2704b23e1d41a36be7b5bb9de81179
Cale Bybee <bybeecale@gmail.com>
2021-12-10 11:57:32 -0700

    setting up fake seeder db data

--- database/seeders/ArticlesTableSeeder.php
+++ database/seeders/ArticlesTableSeeder.php
@@ -4,15 +4,15 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class ArticlesTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(ArticlesTableSeeder::class);
+        \App\Models\Article::factory()->count(30)->create();
     }
 }
